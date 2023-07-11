4133e08f15a092bd80b3e43ae46e71ee524067e2
BSK-98 <nathaniellee20graham@gmail.com>
2023-07-11 11:41:17 -0700

    alloewed hosts

--- backend/main.py
+++ backend/main.py
@@ -17,11 +17,12 @@ app = FastAPI ()
 
 # CORS - Origins
 origins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:4174",
-    "http://localhost:4174",
-    "http://localhost:3000",
+    # "http://localhost:5173",
+    # "http://localhost:5174",
+    # "http://localhost:4174",
+    # "http://localhost:4174",
+    # "http://localhost:3000",
+    "https://mira-wheat.vercel.app"
 ]
 
 # CORS - Middleware
